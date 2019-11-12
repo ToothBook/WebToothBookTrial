f67b8c987232e8a06157d5aa96f75d48517b98cd
rivasge_sd2023@CEACCS.NET <rivasge_sd2023@CEACCS.NET>
2019-11-12 16:48:20 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="white"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="@/assets/toothBookLogo.png"
+          transition="scale-transition"
+          width="80"
+        />
+
+        <!-- <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        /> -->
+        <span class="mr-2" style="color:black;font-weight:bold;font-size:30px">ToothBook</span>
+      </div>
+
+      <v-spacer></v-spacer>
+<!-- 
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn> -->
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
